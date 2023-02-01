594f3acab224d764e9b0fc3093b3619867c5aa5c
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-02-01 17:56:38 +0100

    Elements WIP

--- run_test.sh
+++ run_test.sh
@@ -3,12 +3,14 @@ set -e
 
 rm -rf /tmp/forum-lit-open-dev
 
+# nix-shell https://holochain.love --run "
 hc-scaffold web-app forum-lit-open-dev --setup-nix false --template app --templates-path .templates
-
+# "
+cp -R nix default.nix forum-lit-open-dev
 mv forum-lit-open-dev /tmp
 cd /tmp/forum-lit-open-dev
-cp ../forum-lit/default.nix .
-cp -R ../forum-lit/nix .
+
+set -e
 
 hc-scaffold dna forum 
 
@@ -23,26 +25,26 @@ hc-scaffold collection by-author posts_by_author post
 hc-scaffold collection global all_posts_entry_hash post:EntryHash
 hc-scaffold collection by-author posts_by_author_entry_hash post:EntryHash
 
-hc-scaffold link-type post like --bidireccional false
-hc-scaffold link-type comment like:EntryHash --bidireccional true
-hc-scaffold link-type certificate:EntryHash like --bidireccional false
-hc-scaffold link-type agent:creator post:EntryHash --bidireccional true
+hc-scaffold link-type post like --delete true --bidireccional false
+hc-scaffold link-type comment like:EntryHash --delete true --bidireccional true
+hc-scaffold link-type certificate:EntryHash like --delete false --bidireccional false
+hc-scaffold link-type agent:creator post:EntryHash --delete false --bidireccional true
 
 hc-scaffold zome profiles --coordinator dnas/forum/zomes/coordinator --integrity dnas/forum/zomes/integrity
-cargo add -p profiles --git https://github.com/holochain-open-dev/profiles hc_zome_profiles_coordinator
-echo "extern crate hc_zome_profiles_coordinator;" > dnas/forum/zomes/coordinator/profiles/src/lib.rs
-cargo add -p profiles_integrity --git https://github.com/holochain-open-dev/profiles hc_zome_profiles_integrity
-echo "extern crate hc_zome_profiles_integrity;" > dnas/forum/zomes/integrity/profiles/src/lib.rs
 
 nix-shell . --run "
-set -e
+cargo add -p profiles hc_zome_profiles_coordinator
+echo \"extern crate hc_zome_profiles_coordinator;\" > dnas/forum/zomes/coordinator/profiles/src/lib.rs
+cargo add -p profiles_integrity hc_zome_profiles_integrity
+echo \"extern crate hc_zome_profiles_integrity;\" > dnas/forum/zomes/integrity/profiles/src/lib.rs
+
 npm i
 
 npm i -w ui @holochain-open-dev/file-storage
 hc-scaffold zome file_storage --coordinator dnas/forum/zomes/coordinator --integrity dnas/forum/zomes/integrity
-cargo add -p file_storage --git https://github.com/holochain-open-dev/file-storage hc_zome_file_storage_coordinator
+cargo add -p file_storage hc_zome_file_storage_coordinator
 echo \"extern crate hc_zome_file_storage_coordinator;\" > dnas/forum/zomes/coordinator/file_storage/src/lib.rs
-cargo add -p file_storage_integrity --git https://github.com/holochain-open-dev/file-storage hc_zome_file_storage_integrity
+cargo add -p file_storage_integrity hc_zome_file_storage_integrity
 echo \"extern crate hc_zome_file_storage_integrity;\" > dnas/forum/zomes/integrity/file_storage/src/lib.rs
 
 npm run format -w ui
