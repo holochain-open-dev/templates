6a116c8d3fb8bc27bd4d1a9145c0459b7a28fb09
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-03-28 14:09:07 +0200

    WIP module

--- run_test_module.sh
+++ run_test_module.sh
@@ -15,7 +15,7 @@ nix develop --command bash -c "
 set -e
 hc-scaffold entry-type post --zome posts_integrity --reference-entry-hash false --crud crud --link-from-original-to-each-update true --fields title:String:TextField,needs:Vec\<String\>:TextField
 hc-scaffold entry-type comment --zome posts_integrity  --reference-entry-hash false --crud crud --link-from-original-to-each-update false --fields post_hash:ActionHash::Post
-hc-scaffold entry-type like --zome posts_integrity  --reference-entry-hash false --crud crd --fields like_hash:Option\<ActionHash\>::Like,image_hash:EntryHash:Image,agent:AgentPubKey:SearchAgent
+hc-scaffold entry-type like --zome posts_integrity  --reference-entry-hash false --crud crd --fields like_hash:Option\<ActionHash\>::Like,agent:AgentPubKey:SearchAgent
 hc-scaffold entry-type certificate --zome posts_integrity  --reference-entry-hash true --crud cr --fields post_hash:ActionHash::Post,agent:AgentPubKey::certified,certifications_hashes:Vec\<EntryHash\>::Certificate,certificate_type:Enum::CertificateType:TypeOne.TypeTwo,dna_hash:DnaHash
 
 hc-scaffold collection --zome posts_integrity  global all_posts post 
@@ -28,16 +28,8 @@ hc-scaffold link-type --zome posts_integrity  comment like:EntryHash --delete tr
 hc-scaffold link-type --zome posts_integrity  certificate:EntryHash like --delete false --bidireccional false
 hc-scaffold link-type --zome posts_integrity  agent:creator post:EntryHash --delete false --bidireccional true
 
-hc-scaffold zome file_storage --coordinator dnas/forum/zomes/coordinator --integrity dnas/forum/zomes/integrity
-
-cargo add -p file_storage hc_zome_file_storage_coordinator
-echo \"extern crate hc_zome_file_storage_coordinator;\" > dnas/forum/zomes/coordinator/file_storage/src/lib.rs
-cargo add -p file_storage_integrity hc_zome_file_storage_integrity
-echo \"extern crate hc_zome_file_storage_integrity;\" > dnas/forum/zomes/integrity/file_storage/src/lib.rs
-
 npm i
 
-npm i -w ui @holochain-open-dev/file-storage
 npm start
 npm t
 
