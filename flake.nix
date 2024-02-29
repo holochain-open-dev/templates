4626a754372d63cec44e31cf1665ee173bd45b07
guillem.cordoba <guillem.cordoba@gmail.com>
2024-02-29 16:59:35 +0100

    Fixes

--- flake.nix
+++ flake.nix
@@ -37,45 +37,15 @@
               ];
             };
 
-            packages.hc-scaffold-template-app = pkgs.runCommand "hc-scaffold" {
-              buildInputs = [ pkgs.makeWrapper ];
-              src = ./.;
-            } ''
-              mkdir $out
-              # Link every top-level folder from pkgs.hello to our new target
-              ln -s ${inputs'.scaffolding.packages.default}/* $out
-              # Except the bin folder
-              rm $out/bin
-              mkdir $out/bin
-              # We create the bin folder ourselves and link every binary in it
-              ln -s ${inputs'.scaffolding.packages.default}/bin/* $out/bin
-              # Except the hello binary
-              rm $out/bin/hc-scaffold
-              cp $src/.templates/app -R $out/.template
-              # Because we create this ourself, by creating a wrapper
-              makeWrapper ${inputs'.scaffolding.packages.default}/bin/hc-scaffold $out/bin/hc-scaffold \
-                --add-flags "--template $out/.template"
-            '';
+            packages.hc-scaffold-app-template = inputs.scaffolding.lib.wrapCustomTemplate {
+              inherit pkgs system;
+              customTemplatePath = ./.templates/app;
+            };
 
-            packages.hc-scaffold-template-module= pkgs.runCommand "hc-scaffold" {
-              buildInputs = [ pkgs.makeWrapper ];
-              src = ./.;
-            } ''
-              mkdir $out
-              # Link every top-level folder from pkgs.hello to our new target
-              ln -s ${inputs'.scaffolding.packages.default}/* $out
-              # Except the bin folder
-              rm $out/bin
-              mkdir $out/bin
-              # We create the bin folder ourselves and link every binary in it
-              ln -s ${inputs'.scaffolding.packages.default}/bin/* $out/bin
-              # Except the hello binary
-              rm $out/bin/hc-scaffold
-              cp $src/.templates/module -R $out/template
-              # Because we create this ourself, by creating a wrapper
-              makeWrapper ${inputs'.scaffolding.packages.default}/bin/hc-scaffold $out/bin/hc-scaffold \
-                --add-flags "--template $out/template"
-            '';
+            packages.hc-scaffold-module-template = inputs.scaffolding.lib.wrapCustomTemplate {
+              inherit pkgs system;
+              customTemplatePath = ./.templates/module;
+            };
 
           };
       };
